2eff918423c47321c522bfa1b33df70a3d8caf59
mided3v <ayo4real6@gmail.com>
2023-10-02 11:49:14 +0100

    updated

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const container = document.getElementById("container");
 let item = document.getElementById("object");
 let btnTarget = document.querySelectorAll(".target");
 
-container.style.backgroundColor = "";
+// btnTarget.style.backgroundColor = "red";
 
 container.addEventListener("click", getClickPosition, false);
 
 function getClickPosition(e) {
   var xPosition = e.clientX - item.offsetWidth / 1.5;
-  var yPosition = e.clientY - item.offsetWidth / 0.85;
+  var yPosition = e.clientY - item.offsetWidth / .40;
 
   var translate3dValue =
     "translate3d(" + xPosition + "px," + yPosition + "px, 0)";
